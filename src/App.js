504b91ca6d7a5aa90aef91db89a08deddad6c300
Atharva Nagore <atharvanagore2004@gmail.com>
2023-01-03 16:35:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import PropertyDetails from './pages/PropertyDetails';
+import ScrollToTOp from './components/ScrollToTop'
 
 function App() {
   return (
-    <div className="max-w-[1440px] mx-auto bg-white ">
+    <div className='max-w-[1350px] mx-auto bg-white'>
+    <ScrollToTOp />
       <Header />
       <Routes>
         <Route path="/" element ={<Home />} />
